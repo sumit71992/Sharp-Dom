6e6468fe14eb03e1780818939cfa2bd855424d73
sumit71992 <sumit71992@gmail.com>
2023-02-08 22:29:15 +0530

    store multiple user to local storage

--- booking.js
+++ booking.js
@@ -3,6 +3,7 @@ document.querySelector('form.formdata').addEventListener('submit', function (e) 
     let x = document.querySelector('form.formdata').elements;
     var t = document.getElementById("time");
     var text = t.options[t.selectedIndex].text;
+    let email = x['email'].value;
    let user_obj={
     name: x['name'].value,
     email: x['email'].value,
@@ -14,7 +15,7 @@ document.querySelector('form.formdata').addEventListener('submit', function (e) 
     // localStorage.setItem('email',x['email'].value);
     // localStorage.setItem('phone',x['phone'].value);
     // localStorage.setItem('Time For Call',x['date'].value+" "+text);
-    localStorage.setItem('user',user);
+    localStorage.setItem(email,user);
     let user_object = JSON.parse(localStorage.getItem('user'))
     console.log(user_object)
 });
