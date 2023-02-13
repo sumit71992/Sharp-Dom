a3dd01cf62df832a3db29ad67d3ae7b904cdb5d8
sumit71992 <sumit71992@gmail.com>
2023-02-14 02:47:08 +0530

    created appointment in server

--- booking.js
+++ booking.js
@@ -5,36 +5,27 @@ document
     e.preventDefault();
     let x = document.querySelector("form.formdata").elements;
     var t = document.getElementById("time");
-    var text = t.options[t.selectedIndex].text;
+
     let email = x["email"].value;
     let user_obj = {
       name: x["name"].value,
       email: x["email"].value,
       phone: x["phone"].value,
-      Time: x["date"].value + " " + text,
     };
-    let user = JSON.stringify(user_obj);
-    localStorage.setItem(email, user);
-    window.location.reload();
-  });
-
-let keys = Object.keys(localStorage);
-i = keys.length;
-while (i--) {
-  let div = document.createElement("div");
+    
+    // localStorage.setItem(email, user);
+    axios.post("https://crudcrud.com/api/4cbed566c0c5499695434f9780572a7c/appoinmentData",user_obj)
+    .then((data)=>{
+      let div = document.createElement("div");
   div.className = "d-flex justify-content-between gap-2";
   let p = document.createElement("p");
-  div.setAttribute("id", JSON.parse(localStorage.getItem(keys[i])).email);
-
+  div.setAttribute("id", data.data._id);
   p.appendChild(
-    document.createTextNode(
-      JSON.parse(localStorage.getItem(keys[i])).name +
+    document.createTextNode(data.data.name +
         " " +
-        JSON.parse(localStorage.getItem(keys[i])).email +
+        data.data.email +
         " " +
-        JSON.parse(localStorage.getItem(keys[i])).phone +
-        " " +
-        JSON.parse(localStorage.getItem(keys[i])).Time
+        data.data.phone
     )
   );
   let btn = document.createElement("button");
@@ -47,7 +38,41 @@ while (i--) {
   div.appendChild(btn);
   div.appendChild(btn1);
   di.appendChild(div);
-}
+     
+      
+      
+    }).catch((err)=>console.log(err))
+    // window.location.reload();
+  });
+
+// let keys = Object.keys(localStorage);
+// i = keys.length;
+// while (i--) {
+//   let div = document.createElement("div");
+//   div.className = "d-flex justify-content-between gap-2";
+//   let p = document.createElement("p");
+//   div.setAttribute("id", JSON.parse(localStorage.getItem(keys[i])).email);
+//   console.log(user)
+//   p.appendChild(
+//     document.createTextNode(
+//       user.data.name +
+//         " " +
+//         JSON.parse(localStorage.getItem(keys[i])).email +
+//         " " +
+//         JSON.parse(localStorage.getItem(keys[i])).phone
+//     )
+//   );
+//   let btn = document.createElement("button");
+//   btn.className = "del_button";
+//   btn.appendChild(document.createTextNode("Delete"));
+//   let btn1 = document.createElement("button");
+//   btn1.className = "edit_btn";
+//   btn1.appendChild(document.createTextNode("Edit"));
+//   div.appendChild(p);
+//   div.appendChild(btn);
+//   div.appendChild(btn1);
+//   di.appendChild(div);
+// }
 //delete local storage
 let local_data = document.querySelector(".local");
 local_data.addEventListener("click", delete_data);
