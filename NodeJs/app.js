ef9ea4c067f78d9a3502fa9365c5152e978b3db0
sumit71992 <sumit71992@gmail.com>
2023-02-17 20:36:19 +0530

    route declared

--- NodeJs/app.js
+++ NodeJs/app.js
@@ -1,8 +1,39 @@
 const http = require('http');
 const port = 4000;
 
-const rqListener=(req,res)=>{
-    console.log("Sumit")
+const rqListener = (req, res) => {
+    const url = req.url;
+    res.setHeader('Content-Type', 'text/html');
+    if (url === '/home') {
+        res.write('<html>');
+        res.write('<head><title>Node Page</title></head>');
+        res.write('<body><h1>Welcome home</h1></body>');
+        res.write('</html>');
+        return res.end();
+        
+    }
+    if (url === '/about') {
+        res.write('<html>');
+        res.write('<head><title>Node Page</title></head>');
+        res.write('<body><h1> Welcome to About Us page</h1></body>');
+        res.write('</html>');
+        return res.end();
+        
+    }
+    if (url === '/node') {
+        res.write('<html>');
+        res.write('<head><title>Node Page</title></head>');
+        res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+        res.write('</html>');
+        return res.end();
+       
+    }else{
+    res.write('<html>');
+    res.write('<head><title>Node Page</title></head>');
+    res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+    res.write('</html>');
+    res.end();
+    }
 }
 const server = http.createServer(rqListener);
 server.listen(port);
