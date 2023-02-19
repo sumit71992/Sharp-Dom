4f65ab4ea1982c11d3f77cdc61d1f66374fa7ac6
sumit71992 <sumit71992@gmail.com>
2023-02-19 07:58:53 +0530

    router and filters

--- ExpressJs/app.js
+++ ExpressJs/app.js
@@ -1,21 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop')
 const port = 3000;
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>');
 
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+// 404 error page
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>');
 });
-app.post('/product',(req,res,next)=>{
-    console.log(req.body.title);
-    console.log(req.body.size);
-    res.redirect('/');
-})
-app.use('/',(req,res,next)=>{
-    res.send('<h1>Hello Express!</h1>');
-});
+
 
 app.listen(port);
