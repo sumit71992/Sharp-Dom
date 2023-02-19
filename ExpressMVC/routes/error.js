99791660ea35431b21d61244da3830503f837552
sumit71992 <sumit71992@gmail.com>
2023-02-19 19:52:54 +0530

    controllers

---
+++ ExpressMVC/routes/error.js
@@ -0,0 +1,7 @@
+const errorController = require('../controllers/products')
+const express = require('express');
+const router = express.Router();
+
+router.get('/',);
+
+module.exports = router;
---
+++ ExpressMVC/routes/shop.js
@@ -0,0 +1,7 @@
+const productsController = require('../controllers/products')
+const express = require('express');
+const router = express.Router();
+
+router.get('/',productsController.getProducts);
+
+module.exports = router;
