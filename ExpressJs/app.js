7dc1b1222d3b6fafee707dc62a6c78fac84d3198
sumit71992 <sumit71992@gmail.com>
2023-02-19 07:22:53 +0530

    fetch body data in express

--- ExpressJs/app.js
+++ ExpressJs/app.js
@@ -1,17 +1,21 @@
-const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 const port = 3000;
 
 const app = express();
 
-app.use((req,res,next)=>{
-   
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>');
+
+});
+app.post('/product',(req,res,next)=>{
+    console.log(req.body.title);
+    console.log(req.body.size);
+    res.redirect('/');
+})
+app.use('/',(req,res,next)=>{
+    res.send('<h1>Hello Express!</h1>');
 });
 
-app.use((req,res,next)=>{
-    res.send( { key1: "value" });
-});
-
-const server = http.createServer(app);
-server.listen(port);
+app.listen(port);
