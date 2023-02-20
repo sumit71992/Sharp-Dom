cf10d9ed88dd071086436489d5342a520ead6436
sumit71992 <sumit71992@gmail.com>
2023-02-20 09:01:59 +0530

    read data from file optimized

--- ExpressMVC/modules/productModule.js
+++ ExpressMVC/modules/productModule.js
@@ -1,39 +1,35 @@
 const fs = require("fs");
 const path = require("path");
+
+const getProductFromFile = (cb) => {
+  const location = path.join(
+    path.dirname(process.mainModule.filename),
+    "data",
+    "products.json"
+  );
+  fs.readFile(location, (err, data) => {
+    if (err) {
+      return cb([]);
+    }
+    cb(JSON.parse(data));
+  });
+};
+
 module.exports = class Product {
   constructor(name) {
     this.title = name;
   }
   save() {
-    const location = path.join(
-      path.dirname(process.mainModule.filename),
-      "data",
-      "products.json"
-    );
-    fs.readFile(location, (err, data) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(data);
-      }
+    getProductFromFile((products) => {
       products.push(this);
       fs.writeFile(location, JSON.stringify(products), (err) => {
-        if(err){
-            console.log("err",err);
+        if (err) {
+          console.log("err", err);
         }
       });
     });
   }
   static fetchAll(cb) {
-    const location = path.join(
-        path.dirname(process.mainModule.filename),
-        "data",
-        "products.json"
-      );
-      fs.readFile(location, (err, data)=>{
-        if(err){
-            cb([]);
-        }
-        cb(JSON.parse(data));
-      });
+    getProductFromFile(cb);
   }
 };
