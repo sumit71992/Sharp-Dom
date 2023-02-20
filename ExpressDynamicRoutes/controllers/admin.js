b84075ff9efa3c001ba5d9c96b68a6ca8620cc9e
sumit71992 <sumit71992@gmail.com>
2023-02-20 22:55:22 +0530

    edit product

--- ExpressDynamicRoutes/controllers/admin.js
+++ ExpressDynamicRoutes/controllers/admin.js
@@ -1,12 +1,10 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
+    editing: false
   });
 };
 
@@ -19,6 +17,35 @@ exports.postAddProduct = (req, res, next) => {
   product.save();
   res.redirect('/');
 };
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect('/');
+  }
+  const prodId = req.params.id;
+  Product.fetchProduct(prodId, product => {
+    if (!product) {
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product
+    });
+  })
+};
+exports.postEditProduct = (req, res, next) => {
+  const id = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDecription = req.body.description;
+  const updatedProduct = new Product(id, updatedTitle, updatedImageUrl, updatedDecription, updatedPrice);
+  updatedProduct.save();
+  res.redirect('/admin/products');
+}
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
--- ExpressDynamicRoutes/routes/admin.js
+++ ExpressDynamicRoutes/routes/admin.js
@@ -15,4 +15,7 @@ router.get('/products', adminController.getProducts);
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
+router.get('/edit-product/:id', adminController.getEditProduct);
+router.post('/edit-product', adminController.postEditProduct);
+
 module.exports = router;
